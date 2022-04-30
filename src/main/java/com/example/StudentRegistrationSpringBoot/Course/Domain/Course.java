3a59dbc177c7cd486bda371ae0a4839e94b18e0b
SuadNesimi <suadnesimi12@gmail.com>
2022-04-30 02:18:34 +0200

    Initial Commit

---
+++ src/main/java/com/example/StudentRegistrationSpringBoot/Course/Domain/Course.java
@@ -0,0 +1,30 @@
+package com.example.StudentRegistrationSpringBoot.Course.Domain;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+
+    public Course() {
+
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+
+}
+
+
